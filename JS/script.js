9fe52b95acd1c172344ffde8716cbbd40021233f
Edward <edwrdshen@gmail.com>
2020-01-14 00:02:59 +1100

    carousel & footer

--- JS/script.js
+++ JS/script.js
@@ -34,7 +34,7 @@ barsRef.addEventListener("click", () => {
 })
 
 // Bars animation to & from cross:
-$(document).ready(function () {
+jQuery(function () {
 
     $(".nav-icon").click(function () {
         $(".nav-icon .first").toggleClass('animate1');
@@ -68,10 +68,113 @@ $(document).ready(function () {
 // .nav-icon:before
 // .nav-icon div
 
+// Button References via JS DOM:
+// const left_button = document.querySelector('.fa-chevron-left');
+// const right_button = document.querySelector('.fa-chevron-right');
+
 // Carousel Code:
+// $('tagName').toggleClass('selectorName')
 
-const price_item = ["Basic", "Standard", "Professional"];
+jQuery(function () {
+    const price_item = [".basic", ".standard", ".deluxe"];
+    const mobAnimationNames = ["slide-appear-left-mob", "slide-disappear-left-mob", "slide-appear-right-mob", "slide-disappear-right-mob", "hide"];
+    var viewIndex = 1;
+    // References to every carousel plan:
+    const planItems = document.querySelectorAll('.carousel-item');
 
-// Button References via JS DOM.
-const left_button = document.querySelector('.fa-chevron-left')
-const right_button = document.querySelector('.fa-chevron-right')
+    setInterval(function () {
+        function Increment(indexVal) {
+            if (indexVal != 2) {
+                indexVal = indexVal + 1;
+                return indexVal;
+            }
+            else {
+                return 0;
+            }
+        }
+        function Decrement(indexVal) {
+            if (indexVal != 0) {
+                indexVal = indexVal - 1;
+                return indexVal;
+            }
+            else {
+                return 2;
+            }
+        }
+
+        // Remove Animation Class Elements:
+        $('.basic').removeClass("slide-appear-left-mob slide-disappear-left-mob slide-appear-right-mob slide-disappear-right-mob hide")
+        $('.standard').removeClass("slide-appear-left-mob slide-disappear-left-mob slide-appear-right-mob slide-disappear-right-mob hide")
+        $('.deluxe').removeClass("slide-appear-left-mob slide-disappear-left-mob slide-appear-right-mob slide-disappear-right-mob hide")
+
+        // Decrease viewIndex value.
+        viewIndex = Increment(viewIndex);
+        console.log(viewIndex);
+        $(price_item[viewIndex]).toggleClass('slide-appear-right-mob')
+        $(price_item[Decrement(viewIndex)]).toggleClass('slide-disappear-right-mob')
+        $(price_item[Increment(viewIndex)]).toggleClass('hide')
+    }
+        , 8000);
+
+    $('.fa-chevron-left').click(() => {
+        function Increment(indexVal) {
+            if (indexVal != 2) {
+                indexVal = indexVal + 1;
+                return indexVal;
+            }
+            else {
+                return 0;
+            }
+        }
+        function Decrement(indexVal) {
+            if (indexVal != 0) {
+                indexVal = indexVal - 1;
+                return indexVal;
+            }
+            else {
+                return 2;
+            }
+        }
+        // Remove Animation Class Elements:
+        $('.basic').removeClass("slide-appear-left-mob slide-disappear-left-mob slide-appear-right-mob slide-disappear-right-mob hide")
+        $('.standard').removeClass("slide-appear-left-mob slide-disappear-left-mob slide-appear-right-mob slide-disappear-right-mob hide")
+        $('.deluxe').removeClass("slide-appear-left-mob slide-disappear-left-mob slide-appear-right-mob slide-disappear-right-mob hide")
+        // Decrease viewIndex value.
+        viewIndex = Decrement(viewIndex);
+        console.log(viewIndex);
+        $(price_item[viewIndex]).toggleClass('slide-appear-left-mob')
+        $(price_item[Decrement(viewIndex)]).toggleClass('slide-disappear-left-mob')
+        $(price_item[Increment(viewIndex)]).toggleClass('hide')
+    })
+    $('.fa-chevron-right').click(() => {
+        function Increment(indexVal) {
+            if (indexVal != 2) {
+                indexVal = indexVal + 1;
+                return indexVal;
+            }
+            else {
+                return 0;
+            }
+        }
+        function Decrement(indexVal) {
+            if (indexVal != 0) {
+                indexVal = indexVal - 1;
+                return indexVal;
+            }
+            else {
+                return 2;
+            }
+        }
+        // Remove Animation Class Elements:
+        $('.basic').removeClass("slide-appear-left-mob slide-disappear-left-mob slide-appear-right-mob slide-disappear-right-mob hide")
+        $('.standard').removeClass("slide-appear-left-mob slide-disappear-left-mob slide-appear-right-mob slide-disappear-right-mob hide")
+        $('.deluxe').removeClass("slide-appear-left-mob slide-disappear-left-mob slide-appear-right-mob slide-disappear-right-mob hide")
+
+        // Decrease viewIndex value.
+        viewIndex = Increment(viewIndex);
+        console.log(viewIndex);
+        $(price_item[viewIndex]).toggleClass('slide-appear-right-mob')
+        $(price_item[Decrement(viewIndex)]).toggleClass('slide-disappear-right-mob')
+        $(price_item[Increment(viewIndex)]).toggleClass('hide')
+    })
+});
